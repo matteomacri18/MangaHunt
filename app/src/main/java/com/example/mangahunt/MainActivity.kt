0feec62c16cc429a755b14bddce89858ffaa1bf7
Matteo Macrì <matteomacri18@gmail.com>
2023-09-03 13:52:53 +0200

    Remove dark mode

--- app/src/main/java/com/example/mangahunt/MainActivity.kt
+++ app/src/main/java/com/example/mangahunt/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MangaHuntTheme {
+            MangaHuntTheme(darkTheme = false) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
