f51d132b04f5847f4ed0f63afe6c1dc3d8518559
Matteo Macrì <matteomacri18@gmail.com>
2023-09-23 11:56:54 +0200

    Add components

--- app/src/main/java/com/example/mangahunt/screens/LoginScreen.kt
+++ app/src/main/java/com/example/mangahunt/screens/LoginScreen.kt
@@ -1,12 +1,14 @@
 package com.example.mangahunt.screens
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.mangahunt.navigation.Screen
 
@@ -17,15 +19,20 @@ fun LoginScreen(
     Surface(
         modifier = Modifier
             .fillMaxSize()
+            .padding(24.dp),
     ) {
-        Row {
-            Text(
-                text = "Login Screen",
-                modifier = Modifier
-                    .clickable {
-                        navController.navigate(route = Screen.Home.route)
-                    }
-            )
+        Column {
+            // Logo Image
+
+            Button(
+                onClick = {
+                    navController.navigate(route = Screen.Home.route)
+                }
+            ) {
+                Text(
+                    text = "Login"
+                )
+            }
         }
     }
 }
