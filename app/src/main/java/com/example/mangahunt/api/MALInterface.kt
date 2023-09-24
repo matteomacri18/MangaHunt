056258e483d70c5eb77f42fffe22059ead23b77f
Matteo Macrì <matteomacri18@gmail.com>
2023-09-24 12:45:14 +0200

    Add APIs

--- app/src/main/java/com/example/mangahunt/api/MALInterface.kt
+++ app/src/main/java/com/example/mangahunt/api/MALInterface.kt
@@ -2,17 +2,24 @@ package com.example.mangahunt.api
 
 import com.example.mangahunt.constants.Constants
 import com.example.mangahunt.models.Mangas
-import com.example.mangahunt.models.MangasItem
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
 
+object HeaderContent {
+    const val API_KEY = "X-RapidAPI-Key: ${Constants.API_KEY}"
+    const val HOST_KEY = "X-RapidAPI-Host: ${Constants.HOST_KEY}"
+}
+
 interface MALInterface {
-    @GET("${Constants.BASE_URL}manga/top/all")
-    @Headers(
-        "X-RapidAPI-Key: ${Constants.API_KEY}",
-        "X-RapidAPI-Host: ${Constants.HOST_KEY}"
-    )
+    @GET("${Constants.BASE_URL}manga/top/manga")
+    @Headers("${HeaderContent.API_KEY}", "${HeaderContent.HOST_KEY}")
     suspend fun getTopManga(): Mangas
 
+    @GET("${Constants.BASE_URL}manga/top/bypopularity")
+    @Headers("${HeaderContent.API_KEY}", "${HeaderContent.HOST_KEY}")
+    suspend fun getPopularManga(): Mangas
+
+    @GET("${Constants.BASE_URL}manga/top/favorite")
+    @Headers("${HeaderContent.API_KEY}", "${HeaderContent.HOST_KEY}")
+    suspend fun getFavoriteManga(): Mangas
 }
