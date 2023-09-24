351ceb561deb85e28d2ef1e5c5a65469ee9a9f72
Matteo Macrì <matteomacri18@gmail.com>
2023-09-24 12:47:19 +0200

    Fix image display

--- app/src/main/java/com/example/mangahunt/components/Card.kt
+++ app/src/main/java/com/example/mangahunt/components/Card.kt
@@ -1,7 +1,9 @@
 package com.example.mangahunt.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -10,10 +12,16 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 
@@ -33,12 +41,40 @@ fun Card(
     ) {
         Box(
             modifier = Modifier
-                .size(140.dp, 170.dp)
+                .size(120.dp, 170.dp)
                 .background(Color.White, shape = RoundedCornerShape(12.dp))
-
         ) {
-            GlideImage(model = cover, contentDescription = null)
-            Text(text = title)
+            GlideImage(
+                model = cover,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    ))
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ){
+                Text(
+                    text = title,
+                    style = TextStyle(
+                        color = Color.White
+                    )
+                )
+            }
         }
+
     }
 }
